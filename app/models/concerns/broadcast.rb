bbe3ff0610d028c3a9eabb3f5d9c8ff077cf5f48
Andrew Foster <github@bluemoon.com.au>
2022-06-30 12:17:42 +0100

    Remove unnecessary callback conditional

--- app/models/concerns/broadcast.rb
+++ app/models/concerns/broadcast.rb
@@ -11,7 +11,7 @@ module Broadcast
         locals: { resource: self },
         target: "turbo_stream_#{resource_name}_#{id}"
       )
-    }, if: proc { |resource| resource.previous_changes.any? }
+    }
     after_update_commit lambda {
       broadcast_replace_later_to(
         resource_name,
@@ -19,7 +19,7 @@ module Broadcast
         locals: { action: :show, resource: self, readonly: true, token: form_authenticity_token },
         target: "turbo_stream_show_#{resource_name}_#{id}"
       )
-    }, if: proc { |resource| resource.previous_changes.any? }
+    }
     after_update_commit lambda {
       broadcast_replace_later_to(
         resource_name,
@@ -27,6 +27,6 @@ module Broadcast
         locals: { action: :edit, resource: self, readonly: false, token: form_authenticity_token },
         target: "turbo_stream_edit_#{resource_name}_#{id}"
       )
-    }, if: proc { |resource| resource.previous_changes.any? }
+    }
   end
 end
