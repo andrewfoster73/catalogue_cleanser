9b74695b75184816d68ce5860578abb591c47b1c
Andrew Foster <github@bluemoon.com.au>
2022-10-03 13:41:02 +0100

    Remove debugging

--- app/controllers/concerns/localisation.rb
+++ app/controllers/concerns/localisation.rb
@@ -11,7 +11,6 @@ module Localisation
   private
 
   def use_locale(&action)
-    puts "current_user: #{current_user}, #{current_user&.locale}"
     locale = current_user&.locale || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
